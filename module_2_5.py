bfc2264697e42b746ca0b58fc2d1b11a3bcd690d
Novik-2000 <lyubasha.novikova.2000@mail.ru>
2024-09-21 19:58:36 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,18 +1,16 @@
 def get_matrix(n, m, value):
     matrix = list()
-    for i in value * n:
-        matrix.append(i)
-        for j in (value * n) * m:
-            matrix.append(j)
-            return matrix
+    for i in m:
+        matrix.append( value * i)
+        zero = list()
+        for j in n:
+            zero.append( matrix * j )
+            return zero
 
 
-result1 = get_matrix(2, 2, [10])
-result2 = get_matrix(3, 5, [42])
-result3 = get_matrix(4, 2, [13])
+result1 = get_matrix([2], [2], [10])
+result2 = get_matrix([3], [5], [42])
+result3 = get_matrix([4], [2], [13])
 print(result1)
 print(result2)
 print(result3)
-
-
-
